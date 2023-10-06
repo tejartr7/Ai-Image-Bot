11773a089eea414023cbed27398cd4d9cd1b5d3c
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-10-06 21:05:05 +0530

    Update Footer.jsx

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='footer text-center mt-5 font-bold text-[#6469ff] text-xl uppercase'>
-            <a href="https://github.com/tejartr7">Github </a>
-            <a href="https://www.linkedin.com/in/raghu-teja-reddy-sureddy-8b6721236/"> Linkedin </a>
-            <a href="https://www.instagram.com/tejartr445/"> Instagram</a>
-            <p>©All Rights Reserved</p>
-        </div>
+       <div style={footerStyle} className='footer text-center mt-5 font-bold text-[#6469ff] text-xl uppercase'>
+      <a href="https://twitter.com/raghu_rtr">Twitter </a>
+      <a href="https://www.linkedin.com/in/raghu-teja-reddy-sureddy-8b6721236/"> Linkedin </a>
+      <a href="mailto:codworldrtr7@gmail.com"> Mail </a>
+      <p>©All Rights Reserved</p>
+    </div>
     )
 }
 
